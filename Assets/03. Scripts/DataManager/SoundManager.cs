3cf120e22dd8101a09389725845619c487754939
moonseonu <48229760+moonseonu@users.noreply.github.com>
2026-06-17 18:54:02 +0900

    260617
    사운드 스크립트 작업 완료

--- Assets/03. Scripts/DataManager/SoundManager.cs
+++ Assets/03. Scripts/DataManager/SoundManager.cs
@@ -5,12 +5,18 @@ public class SoundManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    [SerializeField] private Dictionary<string, AudioClip> BGM_Clips;
+    private Dictionary<string, AudioClip> BGM_Clips;
     private Dictionary<string, AudioClip> SFX_Clips;
+
+    private AudioSource BGM;
+    private AudioSource SFX;
 
     private void Awake()
     {
         init();
+
+        //PlayBGM("BGM_Main_Dawn_Loop", BGM);
+        //PlaySFX("SFX_GUI_Button", SFX);
     }
 
     private void init()
@@ -22,6 +28,8 @@ public class SoundManager : MonoBehaviour
     private void Load_BGM()
     {
         AudioClip[] Clips = Resources.LoadAll<AudioClip>("Sound/BGM");
+        BGM = GetComponent<AudioSource>();
+
         BGM_Clips = new Dictionary<string, AudioClip>();
 
         foreach (AudioClip Cl in Clips)
@@ -32,11 +40,12 @@ public class SoundManager : MonoBehaviour
             }
         }
     }
-
     private void Load_SFX()
     {
-        AudioClip[] Clips = Resources.LoadAll<AudioClip>("Sound/BGM");
-        BGM_Clips = new Dictionary<string, AudioClip>();
+        AudioClip[] Clips = Resources.LoadAll<AudioClip>("Sound/SFX");
+        SFX= GetComponent<AudioSource>();
+
+        SFX_Clips = new Dictionary<string, AudioClip>();
 
         foreach (AudioClip Cl in Clips)
         {
@@ -44,6 +53,31 @@ public class SoundManager : MonoBehaviour
             {
                 SFX_Clips.Add(Cl.name, Cl);
             }
+        }
+    }
+
+    //BGM 재생 함수
+    //파일명 입력
+    public void PlayBGM(string name, AudioSource source)
+    {
+
+        if (BGM_Clips.TryGetValue(name, out AudioClip clip))
+        {
+            if (source.clip == clip && source.isPlaying) return;
+            source.clip = clip;
+            source.loop = true;
+            source.Play();
+        }
+    }
+
+
+    //SFX 재생 함수
+    //SFX는 1회만 재생
+    public void PlaySFX(string name, AudioSource source)
+    {
+        if (SFX_Clips.TryGetValue(name, out AudioClip clip))
+        {
+            source.PlayOneShot(clip);
         }
     }
 }
