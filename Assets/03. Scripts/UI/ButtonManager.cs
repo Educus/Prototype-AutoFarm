a628ec5fe85cc910489afda66f8e29517acd90ef
moonseonu <48229760+moonseonu@users.noreply.github.com>
2026-08-13 01:29:27 +0900

    260813
    Datachip Chart 사운드까지만 적용

--- Assets/03. Scripts/UI/ButtonManager.cs
+++ Assets/03. Scripts/UI/ButtonManager.cs
@@ -31,62 +31,15 @@ public class ButtonManager : MonoBehaviour
             //오브젝트 명에 해당 단어가 포함되어 있는지 확인 후 이벤트 등록
             // ToLowerInvariant()를 사용하여 대소문자 구분 없이 비교
             string buttonName = button.gameObject.name.ToLowerInvariant();
-            if (buttonName.Contains("popup"))
+
+            if (buttonName.Contains("gui"))
             {
-                button.onClick.AddListener(() =>
-                {
-                    UIManager.Instance.PlaySFX("SFX_GUI_Button");
-                    UIManager.Instance.OpenPopUpPage();
-                }
-                );
+                GUIButtonInit(buttonName, button);
             }
 
-            else if (buttonName.Contains("buildingmod"))
+            else if (buttonName.Contains("uimanagement"))
             {
-                button.onClick.AddListener(() =>
-                {
-                    UIManager.Instance.PlaySFX("SFX_GUI_Button");
-                    UIManager.Instance.ToggleBuildMod();
-                }
-                );
-            }
-
-            // string으로 버튼 이름 입력 받아서 이벤트 할당
-            // 예: "field", "storage", "barn" 등
-            //LowerInvariant()를 사용하여 대소문자 구분 없이 비교
-            //버튼 오브젝트에는 "field", "storage", "barn" 등의 아이템 이름이 포함되어 있어야 함
-
-            else if (buttonName.Contains("field"))
-            {
-                button.onClick.AddListener(() =>
-                {
-                    int itemID = DataManager.Instance.GetItemIDNumber("field");
-                    UIManager.Instance.PlaySFX("SFX_GUI_Button");
-                    UIManager.Instance.SelectBuildingKit(itemID);
-                }
-                );
-            }
-
-            else if (buttonName.Contains("storage"))
-            {
-                button.onClick.AddListener(() =>
-                {
-                    int itemID = DataManager.Instance.GetItemIDNumber("storage");
-                    UIManager.Instance.PlaySFX("SFX_GUI_Button");
-                    UIManager.Instance.SelectBuildingKit(itemID);
-                }
-                );
-            }
-
-            else if (buttonName.Contains("barn"))
-            {
-                button.onClick.AddListener(() =>
-                {
-                    int itemID = DataManager.Instance.GetItemIDNumber("barn");
-                    UIManager.Instance.PlaySFX("SFX_GUI_Button");
-                    UIManager.Instance.SelectBuildingKit(itemID);
-                }
-                );
+                UIDataChipInit(buttonName, button);
             }
 
             else if (buttonName.Contains("exit"))
@@ -105,4 +58,81 @@ public class ButtonManager : MonoBehaviour
             }
         }
     }
+
+    private void GUIButtonInit(string buttonName, Button button)
+    {
+
+        //모드 진입 버튼
+        if (buttonName.Contains("management"))
+        {
+            button.onClick.AddListener(() =>
+            {
+                UIManager.Instance.PlaySFX("SFX_GUI_Button");
+                UIManager.Instance.OpenPopUpPage();
+            }
+            );
+        }
+
+        else if (buttonName.Contains("buildingmod"))
+        {
+            button.onClick.AddListener(() =>
+            {
+                UIManager.Instance.PlaySFX("SFX_GUI_Button");
+                UIManager.Instance.ToggleBuildMod();
+            }
+            );
+        }
+
+        //건축 키트 선택 버튼
+        // string으로 버튼 이름 입력 받아서 이벤트 할당
+        // 예: "field", "storage", "barn" 등
+        //LowerInvariant()를 사용하여 대소문자 구분 없이 비교
+        //버튼 오브젝트에는 "field", "storage", "barn" 등의 아이템 이름이 포함되어 있어야 함
+        else if (buttonName.Contains("field"))
+        {
+            button.onClick.AddListener(() =>
+            {
+                int itemID = DataManager.Instance.GetItemIDNumber("field");
+                UIManager.Instance.PlaySFX("SFX_GUI_Button");
+                UIManager.Instance.SelectBuildingKit(itemID);
+            }
+            );
+        }
+
+        else if (buttonName.Contains("storage"))
+        {
+            button.onClick.AddListener(() =>
+            {
+                int itemID = DataManager.Instance.GetItemIDNumber("storage");
+                UIManager.Instance.PlaySFX("SFX_GUI_Button");
+                UIManager.Instance.SelectBuildingKit(itemID);
+            }
+            );
+        }
+
+        else if (buttonName.Contains("barn"))
+        {
+            button.onClick.AddListener(() =>
+            {
+                int itemID = DataManager.Instance.GetItemIDNumber("barn");
+                UIManager.Instance.PlaySFX("SFX_GUI_Button");
+                UIManager.Instance.SelectBuildingKit(itemID);
+            }
+            );
+        }
+    }
+
+    private void UIDataChipInit(string buttonName, Button button)
+    {
+        if(buttonName.Contains("chart"))
+        {
+            button.onClick.AddListener(() =>
+            {
+                UIManager.Instance.PlaySFX("SFX_UI_PopUp_DataChip");
+
+            }
+            );
+        }
+    }
+
 }
